2e6e96ad96f8a870cea74e55ef3043199956fc53
Munezero Christian <chamsf5@aol.com>
2023-03-09 22:48:09 +0200

    Add functionality to toggle darkMode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BsMoon } from 'react-icons/bs';
+import { BsMoon, BsSun } from 'react-icons/bs';
 
-const Navbar = () => {
+const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <div className=" bg-white w-full h-16 flex justify-between items-center px-14">
-      <h3 className=" text-2xl font-bold">Where in the world?</h3>
-      <p className="mode">
-        <span>
-          <BsMoon />
-        </span>
-        Dark Mode
+    <div className=" bg-white w-full h-16 flex justify-between items-center px-14 dark:bg-gray-700">
+      <h3 className=" text-2xl font-bold dark:text-white">
+        Where in the world?
+      </h3>
+      <p onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? (
+          <span className="mode dark:text-white">
+            <BsSun /> Light Mode
+          </span>
+        ) : (
+          <span className="mode">
+            <BsMoon /> Dark Mode
+          </span>
+        )}
       </p>
     </div>
   );
