05ac6b75f4349048021d99e050e891f73535216d
Munezero Christian <chamsf5@aol.com>
2023-03-13 09:36:55 +0200

    Add darkMode to a country component

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Country = ({ country }) => {
   const { name, population, region, capital, flag } = country;
   return (
-    <div className=" bg-white dark:bg-gray-700">
-      <img src={flag} alt={name} className=" w-full h-40" />
+    <div className=" bg-white dark:bg-gray-700 rounded-md overflow-hidden">
+      <img src={flag} alt={name} className="w-full h-40 object-cover" />
       <div className="px-4 py-4">
         <h3 className=" text-xl font-bold">{name}</h3>
         <p>
