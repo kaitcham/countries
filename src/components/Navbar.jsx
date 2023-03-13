5c4de4076e13640944d2c9990983f2433078d389
Munezero Christian <chamsf5@aol.com>
2023-03-13 09:35:39 +0200

    Add responsiveness to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { BsMoon, BsSun } from 'react-icons/bs';
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
-    <div className=" bg-white w-full h-16 flex justify-between items-center px-14 dark:bg-gray-700">
-      <h3 className=" text-2xl font-bold dark:text-white">
+    <div className=" bg-white w-full h-16 flex justify-between items-center px-5 sm:px-14 dark:bg-gray-700">
+      <h3 className=" text-base font-bold dark:text-white sm:text-2xl">
         Where in the world?
       </h3>
       <p onClick={() => setDarkMode(!darkMode)}>
